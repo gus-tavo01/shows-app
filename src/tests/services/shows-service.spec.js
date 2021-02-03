a6c4a7e05b09220a2dcdf428ac7d76b11ea0ee39
Gustavo loera <gustavoa.loera01@gmail.com>
2021-02-03 01:01:06 -0800

    add dummy test

---
+++ src/tests/services/shows-service.spec.js
@@ -0,0 +1,9 @@
+describe('shows-service getSomething', () => {
+  test('When is valid, expect true', async () => {
+    // Arrange
+    const expectedResult = 'One';
+
+    // Assert
+    expect('One').toBe(expectedResult);
+  });
+});
