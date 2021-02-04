f8ffe94cb8d5fef89be8c43685580cb28ca94840
Gustavo loera <gustavoa.loera01@gmail.com>
2021-02-03 20:27:47 -0800

    add happy path getMostPopular

--- src/tests/services/shows-service.spec.js
+++ src/tests/services/shows-service.spec.js
@@ -1,9 +1,84 @@
-describe('shows-service getSomething', () => {
+import ShowsService from '../../services/shows-service';
+import ShowsApi from '../../api/shows-api';
+
+jest.mock('../../api/shows-api');
+
+let showsService;
+let mockShowsApi;
+
+beforeAll(() => {
+  mockShowsApi = new ShowsApi();
+  showsService = new ShowsService();
+  showsService.showsApi = mockShowsApi;
+});
+
+describe('shows-service getMostPopular', () => {
+  // clear mock function after each test execution
+  afterEach(() => {
+    mockShowsApi.getPopular.mockReset();
+  });
+
   test('When is valid, expect true', async () => {
     // Arrange
-    const expectedResult = 'One';
+    const page = 1;
+
+    const popularShows = [
+      {
+        id: 8000,
+        name: 'WandaVision',
+        overView: 'New Disney serie',
+        poster_path: '/dasdasdasd.jpg',
+        vote_average: 8.5
+      },
+      {
+        id: 6000,
+        name: 'Cobra kai',
+        overView: 'Karate old serie',
+        poster_path: '/sdasdasd.jpg',
+        vote_average: 9.5
+      }
+    ];
+
+    // mock function
+    const mockGetPopular = {
+      page,
+      results: popularShows
+    };
+     mockShowsApi.getPopular = jest.fn(async () => mockGetPopular);
+
+    const expectedResult = {
+      payload: {
+        results: popularShows,
+        page,
+      },
+      isSuccess: true,
+      errorMessage: null
+    }
+
+    // Act
+    const result = await showsService.getMostPopular(page);
 
     // Assert
-    expect('One').toBe(expectedResult);
+    expect(result).toMatchObject(expectedResult);
   });
 });
+
+// describe('shows-service getTopRated', () => {
+//   test('When is valid, expect true', async () => {
+//     // Arrange
+//     const expectedResult = 'One';
+
+//     // Assert
+//     expect('One').toBe(expectedResult);
+//   });
+// });
+
+// describe('shows-service getTrending', () => {
+//   test('When is valid, expect true', async () => {
+//     // Arrange
+//     const expectedResult = 'One';
+
+//     // Assert
+//     expect('One').toBe(expectedResult);
+//   });
+// });
