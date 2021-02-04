b24469a888efb8bcaa870be1ed0922d6d66b64f6
Gustavo loera <gustavoa.loera01@gmail.com>
2021-02-03 20:31:46 -0800

    add spanish MX as default language

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,2 +1,3 @@
 export const token = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmYmNmMTc4Y2ZiYzA4YzIxYTM3YTg0NWFkYTBkOWMyYyIsInN1YiI6IjYwMWE1OTQ3NjZhMGQzMDAzZTdjMjM4MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.AR9_LA5hdKbFvRfy3yNhYojL85mvRhGRwjb5yRSoRj0';
 export const tmDbUrl = 'https://api.themoviedb.org';
+export const defaultLanguage = 'es-MX';
