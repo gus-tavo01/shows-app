82f0013fd2c40a6aa32d2e4c714030fe0413dd7f
Gustavo loera <gustavoa.loera01@gmail.com>
2021-02-03 21:05:13 -0800

    add missing test scenarios

--- src/tests/services/shows-service.spec.js
+++ src/tests/services/shows-service.spec.js
@@ -62,28 +62,176 @@ describe('shows-service getMostPopular', () => {
     expect(result).toMatchObject(expectedResult);
   });
 
-  // 'When api response has errors, expect an unsuccessful service response'
+  test('When api response has errors, expect an unsuccessful service response', async () => {
+    // Arrange
+    const page = 10;
+    const expectedErrMessage = 'Unsuccessful response';
+
+    // mock function
+    const mockGetPopular = async () => {
+      const error = {
+        errorMessage: expectedErrMessage
+      };
+      throw error;
+    };
+     mockShowsApi.getPopular = jest.fn(mockGetPopular);
+
+    const expectedResult = {
+      payload: null,
+      isSuccess: false,
+      errorMessage: expectedErrMessage
+    }
+
+    // Act
+    const result = await showsService.getMostPopular(page);
+
+    // Assert
+    expect(result).toMatchObject(expectedResult);
+  });
 });
 
-// describe('shows-service getTopRated', () => {
-//   test('When api returns valid data, expect a successful service response', async () => {
-//     // Arrange
-//     const expectedResult = 'One';
+describe('shows-service getTopRated', () => {
+  test('When api returns valid data, expect a successful service response', async () => {
+    // Arrange
+    const page = 1;
 
-//     // Assert
-//     expect('One').toBe(expectedResult);
-//   });
+    const ratedShows = [
+      {
+        id: 6000,
+        name: 'Cobra kai',
+        overView: 'Karate old serie',
+        poster_path: '/sdasdasd.jpg',
+        vote_average: 9.5
+      },
+      {
+        id: 8000,
+        name: 'Bridgeton',
+        overView: 'Boring serie',
+        poster_path: '/dasdasdasd.jpg',
+        vote_average: 5.5
+      },
+    ];
 
-// 'When theres an api error, expect a service response with errors'
-// });
+    // mock function
+    const mockGetTopRated = {
+      page,
+      results: ratedShows
+    };
+     mockShowsApi.getTopRated = jest.fn(async () => mockGetTopRated);
 
-// describe('shows-service getTrending', () => {
-//   test('When api returns valid data, expect a successful service response', async () => {
-//     // Arrange
-//     const expectedResult = 'One';
+    const expectedResult = {
+      payload: {
+        results: ratedShows,
+        page,
+      },
+      isSuccess: true,
+      errorMessage: null
+    }
 
-//     // Assert
-//     expect('One').toBe(expectedResult);
-//   });
-// 'When api returns errors, expect a service response with errors'
-// });
+    // Act
+    const result = await showsService.getTopRated(page);
+
+    // Assert
+    expect(result).toMatchObject(expectedResult);
+  });
+
+  test('When theres an api error, expect a service response with errors', async () => {
+    // Arrange
+    const page = 10;
+    const expectedErrMessage = 'Unsuccessful response';
+
+    // mock function
+    const mockGetTopRated = async () => {
+      const error = {
+        errorMessage: expectedErrMessage
+      };
+      throw error;
+    };
+     mockShowsApi.getTopRated = jest.fn(mockGetTopRated);
+
+    const expectedResult = {
+      payload: null,
+      isSuccess: false,
+      errorMessage: expectedErrMessage
+    }
+
+    // Act
+    const result = await showsService.getTopRated(page);
+
+    // Assert
+    expect(result).toMatchObject(expectedResult);
+  });
+});
+
+describe('shows-service getTrending', () => {
+  test('When api returns valid data, expect a successful service response', async () => {
+    // Arrange
+    const page = 1;
+
+    const ratedShows = [
+      {
+        id: 6000,
+        name: 'Cobra kai',
+        overView: 'Karate old serie',
+        poster_path: '/sdasdasd.jpg',
+        vote_average: 9.5
+      },
+      {
+        id: 8000,
+        name: 'Bridgeton',
+        overView: 'Boring serie',
+        poster_path: '/dasdasdasd.jpg',
+        vote_average: 5.5
+      },
+    ];
+
+    // mock function
+    const mockGetPopular = {
+      page,
+      results: ratedShows
+    };
+     mockShowsApi.getTrending = jest.fn(async () => mockGetPopular);
+
+    const expectedResult = {
+      payload: {
+        results: ratedShows,
+        page,
+      },
+      isSuccess: true,
+      errorMessage: null
+    }
+
+    // Act
+    const result = await showsService.getTrending(page);
+
+    // Assert
+    expect(result).toMatchObject(expectedResult);
+  });
+
+  test('When api returns errors, expect a service response with errors', async () => {
+    // Arrange
+    const page = 10;
+    const expectedErrMessage = 'Unsuccessful response';
+
+    // mock function
+    const mockGetTrending = async () => {
+      const error = {
+        errorMessage: expectedErrMessage
+      };
+      throw error;
+    };
+     mockShowsApi.getTrending = jest.fn(mockGetTrending);
+
+    const expectedResult = {
+      payload: null,
+      isSuccess: false,
+      errorMessage: expectedErrMessage
+    }
+
+    // Act
+    const result = await showsService.getTrending(page);
+
+    // Assert
+    expect(result).toMatchObject(expectedResult);
+  });
+});
